ae12b13a824b60a3545720342c1b961d765cd8de
[  ] <blank.parenthesis@gmail.com>
2022-05-26 18:39:54 +0100

    Make crate::client structs public

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,7 @@ mod messages;
 mod client;
 mod event_handler;
 
-pub use client::Client;
-pub use client::RequestError;
+pub use client::*;
 pub use event_handler::EventHandler;
 pub use messages::*;
 
